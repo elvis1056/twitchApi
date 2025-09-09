aa1a97ca8938ef6a90da4133089ee11011855fb6
elvislin <elvis1056@MacBook-Pro.local>
2025-09-09 16:15:38 +0800

    fix: twitch api and frontend layout

--- twitchScript.js
+++ twitchScript.js
@@ -15,50 +15,57 @@ document.addEventListener("DOMContentLoaded", () => {
   const leftArrow = document.querySelector('.left__arrow');
   const rightArrow = document.querySelector('.right__arrow');
 
-  const clientId = "75ni0m3qjhdw4wh1ucjyh16ltx0mpf";
+  const TWITCH_CLIENT_ID = "75ni0m3qjhdw4wh1ucjyh16ltx0mpf";
+  const TWITCH_BEARER_TOKEN = "Bearer 8hh131bmqkoi790r48fl68mpb5c5qj" // quick expiration of certificates
 
-  function getLiveStreams() {
+  function getStreams() {
     const xhr = new XMLHttpRequest();
-    const apiUrl = `https://api.twitch.tv/kraken/streams/?language=zh`
+    const apiUrl = `https://api.twitch.tv/helix/streams/?language=zh&type=live`
     xhr.open("GET", apiUrl);
-    xhr.setRequestHeader("client-id", clientId);
+    xhr.setRequestHeader("client-id", TWITCH_CLIENT_ID);
+    xhr.setRequestHeader("Authorization", TWITCH_BEARER_TOKEN)
     xhr.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
     xhr.send();
     xhr.onreadystatechange = function () {
       if ( xhr.readyState === 4 && xhr.status === 200 ) {
         let data = JSON.parse(xhr.responseText);
-        creatLiveStreamDom(data.streams)
+        creatLiveStreamDom(data.data)
+        creatRecommendedChannelDom(data.data)
+        creatCarouselCard(data.data.splice(0,5))
       } 
     }
   }
 
-  function getRecommendedChannel() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.twitch.tv/kraken/streams?language=zh&limit=10`);
-    xhr.setRequestHeader('client-id', clientId);
-    xhr.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if ( xhr.readyState === 4 && xhr.status === 200 ) {
-        let data = JSON.parse(xhr.responseText);
-        creatRecommendedChannelDom(data.streams)
-      } 
-    }
-  }
+  // function getRecommendedChannel() {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', `https://api.twitch.tv/helix/streams?language=zh`);
+  //   xhr.setRequestHeader('client-id', TWITCH_CLIENT_ID);
+  //   xhr.setRequestHeader("Authorization", TWITCH_BEARER_TOKEN)
+  //   xhr.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+  //   xhr.send();
+  //   xhr.onreadystatechange = function () {
+  //     if ( xhr.readyState === 4 && xhr.status === 200 ) {
+  //       let data = JSON.parse(xhr.responseText);
+  //       creatRecommendedChannelDom(data.data)
+  //       creatCarouselCard(data.data.splice(0,5))
+  //     } 
+  //   }
+  // }
 
-  function requestClips() {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', `https://api.twitch.tv/kraken/streams/featured?limit=5`);
-    xhr.setRequestHeader('client-id', clientId);
-    xhr.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
-    xhr.send();
-    xhr.onreadystatechange = function () {
-      if ( xhr.readyState === 4 && xhr.status === 200 ) {
-        let data = JSON.parse(xhr.responseText);
-        creatCarouselCard(data.featured)
-      } 
-    }
-  }
+  // function requestClips() {
+  //   const xhr = new XMLHttpRequest();
+  //   xhr.open('GET', `https://api.twitch.tv/kraken/streams/featured?limit=5`);
+  //   xhr.setRequestHeader('client-id', TWITCH_CLIENT_ID);
+  //   xhr.setRequestHeader("Authorization", TWITCH_BEARER_TOKEN)
+  //   xhr.setRequestHeader('Accept', 'application/vnd.twitchtv.v5+json');
+  //   xhr.send();
+  //   xhr.onreadystatechange = function () {
+  //     if ( xhr.readyState === 4 && xhr.status === 200 ) {
+  //       let data = JSON.parse(xhr.responseText);
+  //       creatCarouselCard(data.featured)
+  //     } 
+  //   }
+  // }
 
   function creatLiveStreamDom(data) {
     data.forEach((item, i) => {
@@ -66,23 +73,23 @@ document.addEventListener("DOMContentLoaded", () => {
       element.className = 'liveStream';
       element.innerHTML = `
         <div class="liveStream__img__wrapper">
-          <a href=${item.channel.url}>
-            <img src=${item.preview.large} />
+          <a href="https://www.twitch.tv/${item.user_login}">
+            <img src=${item.thumbnail_url.replace("{width}", 440).replace("{height}", 248)} />
           </a>
         </div>
         <div class="liveStream__card">
           <div class="liveStream__headShot">
-            <img src=${item.channel.logo} />
+            <img src=${item.thumbnail_url.replace("{width}", 440).replace("{height}", 248)} />
           </div>
           <div class="liveStream__textSection">
-            <a href=${item.channel.url}>
-              <div class="liveStream__text__status">${item.channel.status}</div>
+            <a href="https://www.twitch.tv/${item.user_login}">
+              <div class="liveStream__text__status">${item.title}</div>
             </a>
-            <a href="${item.channel.url}/videos/all">
-              <div class="liveStream__text__name">${item.channel.display_name}</div>
+            <a href="https://www.twitch.tv/${item.user_login}/videos">
+              <div class="liveStream__text__name">${item.user_name}</div>
             </a>
-            <a href="https://www.twitch.tv/directory/game/${item.channel.game}">
-              <div class="liveStream__text__game">${item.channel.game}</div>
+            <a href="https://www.twitch.tv/directory/category${toSlug(item.game_name)}">
+              <div class="liveStream__text__game">${item.game_name}</div>
             </a>
           </div>
         </div>
@@ -97,16 +104,16 @@ document.addEventListener("DOMContentLoaded", () => {
       const element = document.createElement('div');
       element.className = 'sideBar__row__card';
       element.innerHTML = `
-        <a href=${item.channel.url}>
+        <a href="https://www.twitch.tv/${item.user_login}">
           <div class="sideBar__row__card__headShot">
-            <img src=${item.channel.logo} />
+            <img src=${item.thumbnail_url.replace("{width}", 440).replace("{height}", 248)}  />
           </div>
           <div class="sideBar__row__card__text">
-            <div>${item.channel.display_name}</div>
-            <div>${item.game}</div>
+            <div>${item.user_name}</div>
+            <div class="card__text__name">${item.game_name}</div>
           </div>
           <div class="online marginRight5"></div>
-          <div class="sideBar__row__card__num">${item.viewers}</div>
+          <div class="sideBar__row__card__num">${item.viewer_count}</div>
         </a>
       `
       
@@ -121,14 +128,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('.center .video').setAttribute("style", "height: 360px");
     options.channel = centerID;
     const player = new Twitch.Player(centerID, options);
-    // console.log(player)
     player.play();
     player.setVolume(0.5);
     return player;
   }
 
   function stopCarousel(left, right) {
-    console.log(left, right)
     if (left) {
       const leftChannelId = document.querySelector('.left__first__card .video');
       document.querySelector('.left__first__card .view > img').classList.remove('none');
@@ -144,26 +149,29 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  function toSlug(name) {
+    return name.toLowerCase().replace(/\s+/g, "-");
+  }
+
   function creatCarouselCard(data) {
     data.forEach((item, i) => {
-      options.channel = item.stream.channel._id;
+      options.channel = item.user_login;
       carouselCards[i].innerHTML = `
         <div class="view">
-          <div class="video" id="${item.stream.channel.name}"></div>
-          <img src=${item.stream.preview.large} />
+          <div class="video" id="${item.user_login}"></div>
+          <img src=${item.thumbnail_url.replace("{width}", 440).replace("{height}", 248)} />
           <div class="info">
             <div class="info__block">
               <div class="info__avatar">
-                <img src="${item.stream.channel.logo}" />
+                <img src="${item.thumbnail_url.replace("{width}", 440).replace("{height}", 248)}" />
               </div>
               <div class="info__text">
-                <a href="${item.stream.channel.url}">${item.stream.channel.name}</a>
-                <a href="#" >${item.stream.game}</a>
-                <div>${item.stream.viewers} viewers</div>
+                <a href="${item.game_id}">${item.user_name}</a>
+                <a href="https://www.twitch.tv/directory/category${toSlug(item.game_name)}" >${item.game_name}</a>
+                <div>${item.viewer_count} viewers</div>
               </div>
             </div>
-            <div class="info__tag">${item.stream.channel.broadcaster_language}</div>
-            <div class="info__des">${item.stream.channel.description}</div>
+            ${item.tags.map(tag => `<div class="info__tag">${tag}</div>`)}
           </div>
         </div>
       `
@@ -227,8 +235,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 
-  getLiveStreams()
-  getRecommendedChannel()
-  requestClips()
-
+  getStreams()
+  // getRecommendedChannel()
+  // requestClips()
 })
